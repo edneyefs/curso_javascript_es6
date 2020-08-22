84601917c0ae8f085f75a738c248ec277c9a74c1
Edney Ferreira Silva <edneysilva20@hotmail.com>
2020-08-22 00:51:38 -0300

    Aprendendo a utilização dos Operadores em JS

---
+++ operadores/aritméticos/1-aritmeticos.js
@@ -0,0 +1,25 @@
+//Modulo(%)
+//Operador binário. Retorna o inteiro restante da divisão dos dois operando.
+
+12 % 5 //retorna 2.
+
+//Incremento(++) e decremento(--)
+++x
+x++
+
+--x
+x--
+
+//Negação (-) e adição (+)
+-3
++"3"    //retona 3
++true   //retorna 1
++false  //retorna 0
+-true   //retorna -1
+
+//operador de exponenciação(**)
+2**3    //retorna 8
+10**-1  //retorna 0.1
+
+//Operador de agrupamento
+2 * (3 + 2)
---
+++ operadores/comparação/1-comparacao.js
@@ -0,0 +1,36 @@
+// Igual (==)
+//Retorna verdadeiro caso os operandos sejam iguais. 3 == var1
+"3" == var1
+3 == "3"
+
+//Não igual (!=)
+//Retorna verdadeiro caso os perandos não sejam iguais. var1 != 4
+var2 != "3"
+
+//Estritamente igual(===)
+//Retorna verdadeiro caso os operandos sejam iguais e do mesmo tipo. Veja também  Object.is e igualdade
+3 === var1
+
+//Estritamente não igual(!==)
+//Retorna verdadeiro caso os operandos não sejam iguais e/ou não sejam do mesmo tipo.
+3 !== var1
+
+//Maior que(>)
+//Retorna verdadeiro caso o operando da esqueda seja maior que o da direita.
+var2 > var1
+"12" > 2
+
+//Maior ou igual que(>=)
+//Retorna verdadeiro caso o operando da esqueda seja maior ou igual ao da direita.
+var2 >= var1
+"12" >= 3
+
+//Menor que(>)
+//Retorna verdadeiro caso o operando da esqueda seja menor que o da direita.
+var1 < var2
+"12" < 2
+
+//Menor ou igual que(<=)
+//Retorna verdadeiro caso o operando da esqueda seja menor ou igual ao da direita.
+var2 <= var1
+"12" <= 3
