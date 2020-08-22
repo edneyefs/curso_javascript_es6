ef271dc1149682fafdb7a7727ee8819bb16b3d12
Edney Ferreira Silva <edneysilva20@hotmail.com>
2020-08-21 21:40:29 -0300

    Aprendendo as formas Number

---
+++ tipos/number/1-number.js
@@ -0,0 +1,15 @@
+const myNumber = 12.4032;
+
+//Transforma número para string
+const numberAsStrig = myNuber.toString();
+console.log('Número transformado em string: ', typeod numberAsStrig);
+
+//Retorna número com um número de casas decimais
+const fixedTwoDecimals = myNuber.toFixed(2);
+console.log('\nNúmero com duas casas decimais:', fixedTwoDecimals);
+
+//Transforma uma string em float
+console.log('\nString parseada para float:', parseFloat('13.22'));
+
+//Trasforma uma string em int
+console.log('\nString parseada para int:', parseInt('13.20'));
